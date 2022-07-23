85745264db08c44b5c30a3b6d7f0cbc16519e903
unknown <Naveed Jameel>
2022-07-23 02:44:43 -0700

    lesson6 complete

--- course2/lesson6-Hardhat/hardhat.config.js
+++ course2/lesson6-Hardhat/hardhat.config.js
@@ -2,14 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan"); // this npm pkj for contract verification
 require("./tasks/block-number");
-
-/** @type import('hardhat/config').HardhatUserConfig */
-
-
-const RPC_URL = process.env.RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-
+require("hardhat-gas-reporter"); // when test run this work
+require("solidity-coverage"); // npm pkj >run> y hh coverage
+const RPC_URL = process.env.RPC_URL; // of blockchain network like mainnet,rinkeby etc
+const PRIVATE_KEY = process.env.PRIVATE_KEY; // private of metamask wallet/account
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY; // my app key that i create on etherscan.io 
 module.exports = {
   solidity: "0.8.7",
   defaultNetwork: "hardhat",
@@ -19,8 +16,17 @@ module.exports = {
       accounts: [PRIVATE_KEY],
       chainId: 4
     },
-    etherscan: {
+    etherscan: { // show tx history on etherscan i think
       apiKey: ETHERSCAN_API_KEY,
+    },
+    // below for gas fee of every tx in usd
+    gasReporter: {
+      enabled: true,
+      outputFile: "gas-reporter.txt", // generate this file of gas fee info
+      noColors: true,
+      currency: "USD", // below 2 lines can be remove i think 
+      coinmarketcap: "coinmarket-api-key", // from coinmarketcap login and get key
+      //token:"MATIC"
     }
   }
 };
